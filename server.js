2a06ee9e82d3a52281c377a6a2bb3a565adbd19d
willsonanderson25-alt <willsonanderson25@gmail.com>
2026-09-08 20:12:11 -0800

    Add Express-like res.status/json/send/redirect wrappers for API handler

--- server.js
+++ server.js
@@ -6,6 +6,34 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify({ status: "ok" }));
   }
+
+  // Wrap native res to provide Express-like .status() and .json()
+  res.status = function (code) {
+    res.statusCode = code;
+    return res;
+  };
+  res.json = function (data) {
+    if (!res.getHeader("Content-Type")) {
+      res.setHeader("Content-Type", "application/json");
+    }
+    res.end(JSON.stringify(data));
+  };
+  res.send = function (data) {
+    if (typeof data === "string" && !res.getHeader("Content-Type")) {
+      res.setHeader("Content-Type", "text/plain");
+    }
+    if (Buffer.isBuffer(data)) {
+      if (!res.getHeader("Content-Type")) {
+        res.setHeader("Content-Type", "application/octet-stream");
+      }
+    }
+    res.end(data);
+  };
+  res.redirect = function (url) {
+    res.writeHead(302, { Location: url });
+    res.end();
+  };
+
   return apiHandler(req, res);
 });
 
